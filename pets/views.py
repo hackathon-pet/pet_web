e9a492f8d39f9530a8b9b911ad62c32249eed769
Kyungwon <bkw1103@snu.ac.kr>
2021-07-11 16:36:07 +0900

    show,showpet,updatepet,follow

--- pets/views.py
+++ pets/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Pet, Follow
+from django.http import JsonResponse
 
 def newpet(request):
     return render(request, 'pets/newpet.html')
@@ -19,3 +20,17 @@ def deletepet(request):
   pass
 def updatepet(request):
   pass
+
+class FollowView:
+    def create(request, id):
+        pet = Pet.objects.get(id=id)
+        follow_status = pet.follower_users.filter(user_id=request.user.id)
+        if follow_status.count() > 0:
+            pet.follower_users.get(user=request.user).delete()
+        else:
+            Follow.objects.create(user=request.user, pet=pet)
+        return JsonResponse({
+            'followStatus': follow_status.count(), 
+            'petFollowCount': pet.like_set.count(), 
+            'userFollowCount': request.user.follow_pet.count()
+        })
