45b27c5fdb40eca007e40d3eba1233009fbf6910
keepTaeBorn <samjune1123@gmail.com>
2021-07-11 20:55:15 +0900

    commit

--- petPosts/views.py
+++ petPosts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Post, Photo, Comment, Like, CommentLike
 from accounts.models import Profile
+from pets.models import Pet
 from django.db.models import Count, Sum
 from django.contrib.auth.models import User
 from django.http import JsonResponse
@@ -9,7 +10,7 @@ from django.http import JsonResponse
 def index(request):
     if request.method == 'GET': 
         pet_and_rank={}
-        for pet in Pet.objects.all:
+        for pet in Pet.objects.all():
             pet_and_rank.add({pet, pet.post_set})
         pets_by_ranking={}
         for each in pet_and_rank:
@@ -19,9 +20,8 @@ def index(request):
                 sumlike+=Count(post.like_users)
             pets_by_ranking.add({pet, sumlike})
         sorted(pets_by_ranking.items(), reverse=True)
-        current_pet=Pet.objects.filter(id=pet.id)
-        feed = Post.objects.filter(pet__in=current_pet.following_pets.all()).order_by('-created_at')
-        following_pet=current_pet.following_pets.all()
+        feed = Post.objects.filter(pet__in=request.user.following_pets.all()).order_by('-created_at')
+        following_pet=request.user.following_pets.all()
         comment_form = CommentForm()
         return render(
             request, 
@@ -36,7 +36,7 @@ def index(request):
     elif request.method == 'POST': 
         title = request.POST['title']
         content = request.POST['content']
-        post = Post.objects.create(title=title, content=content, author=request.user)
+        post = Post.objects.create(title=title, content=content)
         for img in request.FILES.getlist('imgs'):
             photo = Photo()
             photo.post = post
