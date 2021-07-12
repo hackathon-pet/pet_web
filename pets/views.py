9bfab393d46c3b0b1f614de033cc00e537494128
Kyungwon <bkw1103@snu.ac.kr>
2021-07-12 23:18:49 +0900

    why can't updatepet?

--- pets/views.py
+++ pets/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Pet, PetForm, Follow
+from django.db.models import Count
 from django.http import JsonResponse
 from django.http import HttpResponseRedirect
 
@@ -20,10 +21,24 @@ def showpet(request, id):
   pet = Pet.objects.get(id=id)
   return render(request, 'pets/showpet.html', {'pet':pet})
 
-def deletepet(request):
-  pass
-def updatepet(request):
-  pass
+def deletepet(request, id):
+  pet = Pet.objects.get(id=id)
+  pet.delete()
+  return redirect('/accounts/myinfo')
+
+def updatepet(request, id):
+  if request.method == 'GET':
+    pet = Pet.objects.get(id=id)
+    return render(request, 'pets/updatepet.html', {'pet':pet})
+  elif request.method == 'POST':
+    pet = Pet.objects.filter(id=id)
+    name = request.POST['name']
+    category = request.POST['category']
+    image = request.FILES['image']
+    introduction = request.POST['introduction']
+    owner=request.user.profile
+    pet.update(name=name, image=image, introduction=introduction, owner=owner, category = category)
+    return redirect('pets:showpet', id=id)
 
 class FollowView:
   def create(request, id):
@@ -34,4 +49,3 @@ class FollowView:
     else:
       Follow.objects.create(user=request.user, pet=pet)
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-
