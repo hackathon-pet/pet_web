897fc45e0df0b59aee6c54315ff0d0df54256b02
Kyungwon <bkw1103@snu.ac.kr>
2021-07-19 21:31:55 +0900

    d

--- pets/static/pets/js/show.js
+++ pets/static/pets/js/show.js
@@ -14,7 +14,8 @@ const follow = async (petID) => {
       const response = await axios.delete(`/pets/${petID}/delete/`);
       location.replace(`/accounts/myinfo/`);
     }
-
+  }
+  
   const nofollow=async() =>{
     alert('팔로우를 하려면 회원가입 또는 로그인을 하세요');
   }
